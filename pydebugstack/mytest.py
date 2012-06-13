483f56b4d8a0bca3e25e1fc311547f6cbfd9a24b
JohnTESlade <john@jtes.net>
2012-06-13 23:40:27 +0100

    Fix tab spaces

--- pydebugstack/mytest.py
+++ pydebugstack/mytest.py
@@ -7,43 +7,44 @@ import logging
 
 class MyTestBase:
 
-	def __init__(self):
-		raise NotImplementedError
+    def add(self, item):
+        raise NotImplementedError
 
-	def add(self, item):
-		raise NotImplementedError
-
-	def get(self):
-		raise NotImplementedError
-	
-	def inc(self, val):
-		raise NotImplementedError
+    def get(self):
+        raise NotImplementedError
     
-	def inc_by_1(self):
-		raise NotImplementedError
+    def inc(self, val):
+        raise NotImplementedError
+    
+    def inc_by_1(self):
+        raise NotImplementedError
 
-	def equal(self):
-		raise NotImplementedError
-
+    def equal(self):
+        raise NotImplementedError
 
 class MyTest(MyTestBase):
+    """ test """
 
-	def __init__(self):
-		self.a = []
-		self.b = 1
-		self.c = 2
+    def __init__(self):
+        self.a = []
+        self.b = 1
+        self.c = 2
 
-	def add(self, item):
-		self.a.append(item)
+    def add(self, item):
+        self.a.append(item)
 
-	def get(self):
-		return self.a
-	
-	def inc(self, val):
-		self.b += val
+    def get(self):
+        return self.a
     
-	def inc_by_1(self):
-		self.inc(1)
+    def inc(self, val):
+        self.b += val
+    
+    def inc_by_1(self):
+        self.inc(1)
+
+    def equal(self):
+        return self.b == self.c
+
 class MyTest2(MyTestBase):
     """ Class that has no main constructor """
 
