22c54ed25e196dd125b0477bd06ea70aa8e0db1e
JohnTESlade <john@jtes.net>
2012-08-16 08:26:23 +0100

    Strip out tests that are "bad"

--- pycharacterize/objects_list.py
+++ pycharacterize/objects_list.py
@@ -32,11 +32,30 @@ class ObjectsList():
 
         logging.debug("Run has finished")
 
+        self.remove_bad_tests()
         self.remove_dups()
 
 
+    def remove_bad_tests(self):
+        """ Removes any bad calls """
+
+        # Currently looks for tests that have no call to __init___
+        # TODO work out where this can actually happen that is useful 
+
+        bad_tests = []
+
+        for object_state in self.object_state_list:
+            if len(filter(lambda x: x['type'] == 'func_call' and x['func'] == "__init__", object_state.call_trace)) == 0:
+                print "Found bad test: {}".format(object_state.call_trace)
+                bad_tests.append(object_state)
+
+        for bad in bad_tests:
+            self.object_state_list.remove(bad)
+
     def remove_dups(self):
         """ Remove duplicates from object lists """
+        
+
         for object_state in self.object_state_list:
 
             logging.debug("State = {}".format(object_state.call_trace))
