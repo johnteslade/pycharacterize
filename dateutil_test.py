f1bbb991d42e138e6c22b914e4a4680b2cb966f2
JohnTESlade <john@jtes.net>
2012-09-03 22:44:06 +0100

    Additional more complex tests

--- dateutil_test.py
+++ dateutil_test.py
@@ -4,24 +4,44 @@ import linecache
 import pycharacterize 
 import yum
 import logging
+import dateutil
 
 
 def dateutil_test():
 
     
     import dateutil.parser
-
+ 
     p = dateutil.parser.parser()
 
     a = p.parse("1-Jan-2012")
-
     print a
+    
+    a = p.parse("3rd March 1978")
+    print a
+    
+    a = p.parse("23-08-1960")
+    print a
+
+#    p1 = dateutil.parser.parser()
+#    a1 = p1.parse("1-Jan-2012")
+#    print a1
+#    
+#    p2 = dateutil.parser.parser()
+#    a2 = p2.parse("3rd March 1978")
+#    print a2
+#    
+#    p3 = dateutil.parser.parser()
+#    a3 = p3.parse("23-08-1960")
+#    print a3
 
 
 if __name__ == "__main__":
-   
+  
+    dateutil_test()
+
     #logging.basicConfig(level=logging.INFO)
-    #logging.basicConfig(level=logging.DEBUG)
+    logging.basicConfig(level=logging.DEBUG)
 
     sys.path.insert(0, '/usr/share/yum-cli')
     import yummain
@@ -36,16 +56,14 @@ if __name__ == "__main__":
     print pdb_obj.all_calls.items()
     print pdb_obj.class_counts.items()
 
-    exit()
 
+    # Create tests
 
+    pdb_obj = pycharacterize.runner.TestPdb(step_all=True)
    
-    pdb_obj.set_class_to_watch(yum.config.BoolOption, "yum.config.BoolOption")
+    pdb_obj.set_class_to_watch(dateutil.parser.parserinfo, "dateutil.parser.parserinfo")
 
-#    pdb_obj.do_runcall(yummain.user_main, ['info', 'firefox'], exit_code=True)
-    pdb_obj.do_runcall(yummain.user_main, [], exit_code=True)
-
-
+    pdb_obj.do_runcall(dateutil_test)
 
 
     print
@@ -62,7 +80,7 @@ if __name__ == "__main__":
     print 
 
 
-    pdb_obj.output_test_code_to_file("testcases_yum.py", backtrace=True)
+    pdb_obj.output_test_code_to_file("testcases_dateutil.py", backtrace=True)
 
     print
     print "!!!!!!!!!!!!!!!!!!! RUNNING THE AUTOGEN ----------------"
